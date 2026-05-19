1393162892ae8e1c2f5fec15af157d43d6da2d51
Sirangu Purnima <sirangupurnima@gmail.com>
2026-05-19 16:57:40 +0530

    meta data fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,9 +14,9 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "AI Ecosystem Portfolio",
-  description: "Next-gen Cloud Architecture Deployment Showcase",
-  // 🆕 Integrated your Google Search Console verification code safely here
+  title: "Purnima Sirangu Portfolio",
+  description: "B.Tech AI & Data Science Student | Full-Stack Developer & AI Engineer specializing in Next.js, Firebase, and Intelligent Systems.",
+
   verification: {
     google: "kM6RjFintIF-6FRFBZsvBgq-7s0TxYRw_I-AplX7Al8",
   },
